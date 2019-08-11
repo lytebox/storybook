38c43a49b032aceec857242d87bd16186d38b0dc
Hizkia Felix <hizkifw@gmail.com>
2019-08-11 15:46:58 +0700

    Add stories for Button component

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,16 +2,25 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
 
-import { Button } from "../components-shared";
+import { Button, FullWidthButton } from "../components-shared";
 
 storiesOf("Button", module)
-  .add("default", () => <Button>Hello Button</Button>)
+  .add("default", () => <Button>Default button</Button>)
+  .add("large", () => <Button large>Large button</Button>)
+  .add("inverted", () => <Button invert>Inverted button</Button>)
+  .add("inverted outline", () => (
+    <Button invert outline>
+      Inverted button
+    </Button>
+  ))
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
+  ))
+  .add("full width", () => (
+    <FullWidthButton>Full width button!</FullWidthButton>
   ));
